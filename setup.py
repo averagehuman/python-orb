c59697f9c1afa58b68ce31007dd2accdd40efddc
podados <gflanagan@devopsni.com>
2012-02-12 18:19:37 +0000

    simplified

--- setup.py
+++ setup.py
@@ -3,28 +3,28 @@
 import os
 from distutils.core import setup
 
-__version__ = '0.7.1'
+__version__ = '0.8'
 projectdir = os.path.dirname(os.path.abspath(__file__))
+readme = open("README").read()
 
-requires = []
 
 setup(
     name="orb",
     version=__version__,
     description="pip/virtualenv shell script wrapper",
-    author="Walter Wefft",
-    author_email = "walter@wefft.com",
+    long_description=readme,
+    author="Djord Flanagan",
+    author_email = "contact@devopsni.com",
     classifiers=[
                 "Intended Audience :: Developers",
                 "Programming Language :: Python",
                 ],
-    requires=requires,
-    url="https://wefft.codebasehq.com/lib/orb",
+    url="https://github.com/podados/python-orb",
     license="BSD",
     download_url="http://pypi.python.org/packages/source/o/orb/orb-%s.tar.gz" % __version__,
     scripts=[
         os.path.join(projectdir, 'orb'),
-        ]
+    ]
 )
     
 
