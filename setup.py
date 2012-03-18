692c2cfa99c3da9f8a3d01bba31ad9d45dbca3a6
podados <gflanagan@devopsni.com>
2012-03-18 16:13:42 +0000

    remove redundant help

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 import os
 from distutils.core import setup
 
-__version__ = '0.9.2'
+__version__ = '0.9.3'
 projectdir = os.path.dirname(os.path.abspath(__file__))
 readme = open("README").read()
 
